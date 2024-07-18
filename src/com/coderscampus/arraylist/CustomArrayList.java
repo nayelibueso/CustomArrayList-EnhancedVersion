aadf4f6d5e519afc264a3ccf018da8deba31f168
nayelibueso <nayelibueso50@gmail.com>
2024-07-17 23:36:46 -0500

    Write assertEquals methods for 4 methods in CustamArrayList class

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -22,7 +22,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 		} if (size == items.length) {
 			resizeArray();
 		}
-		System.arraycopy(item, index, item, index + 1, size - index);
+		System.arraycopy(items, index, items, index + 1, size - index);
 		items [index] = item;
 		size++;
 		return true; 
@@ -49,7 +49,13 @@ public class CustomArrayList<T> implements CustomList<T> {
 		if (index >= size || index <0) {
 			throw new IndexOutOfBoundsException("Index: " + index + " Size: " + size);
 		}
-		return null;
+		T removedItem = (T) items[index];
+		int numMoved = size - index - 1;
+		if(numMoved > 0 ) {
+			System.arraycopy(items, index + 1, items, index, numMoved);
+		}
+		items[--size] = null;
+		return removedItem;
 	}
 	
 	private void resizeArray() { //
--- test/com/coderscampus/arraylist/CustomArrayListTest.java
+++ test/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -1,5 +1,10 @@
 package com.coderscampus.arraylist;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CustomArrayListTest {
 //Code will not be needed anymore as this was the manual way to test merhods. 
 //	public static void main(String[] args) {
@@ -13,5 +18,53 @@ public class CustomArrayListTest {
 //			System.out.println("Element at index " + i + ": " + list.get(i));
 //		}
 //	}
+	
+	private CustomArrayList<Integer> list;
+	
+	@BeforeEach //This annotetion designates a method that should be run before each test metod in the test class.
+	void set_Up () {
+		list = new CustomArrayList<Integer>();
+	}
+	
+	@Test 
+	void test_And_Get_Size () { //Test that elements can be added to the list and size updates correctly. 
+		for (int i = 0; i < 15; i++) {
+			list.add(i);
+		}
+		assertEquals(15, list.getSize());
+	}
+	
+	@Test 
+	void test_Get_Elements () { //Test that elemnts can be retreived correcctly from the list
+		for (int i = 0; i < 15; i++) {
+			list.add(i);
+		}
+		for (int i = 0; i < list .getSize(); i++) { //Iterate over the list and asser each element is equal to its index
+			assertEquals(i, list.get(i));
+		}
+	}
+	
+	@Test
+	void test_Add_At_Index () {
+		list.add(1);
+		list.add(2);
+		list.add(1, 3); //Add element 3 at index 1
+		assertEquals(3, list.getSize());
+		assertEquals(1, list.get(0));
+		assertEquals(3, list.get(1));
+		assertEquals(2, list.get(2));
+	}
+	
+	@Test
+	void test_Remove () {
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		int removed = list.remove(1);
+		assertEquals(2, removed);
+		assertEquals(2, list.getSize());
+		assertEquals(1, list.get(0));
+		assertEquals(3, list.get(1));
+	}
 	
 }
